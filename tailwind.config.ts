a5ed63d889b6a778fbe664abec4f814e30282a12
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-02-10 22:55:36 -0500

    Add @diverse-ui to projects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import withKIT from '@castrogarciajs/material-design'
+import withMD from '@diverse-ui/material-design'
 
-export default withKIT({
+export default withMD({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
     './components/**/*.{js,ts,jsx,tsx,md,mdx}',
